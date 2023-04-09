6026c7ea825d7362659bc00ebe5795d0564401f8
Rachit Joshi <114751447+Rachit2912@users.noreply.github.com>
2023-04-09 18:37:56 +0530

    Update 1281.cpp

--- 1281.cpp
+++ 1281.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     int subtractProductAndSum(int n) {
-        int d=0,p=1,s=0;
-        while(n>0) {
-            d=n%10;
-            s+=d;
-            p*=d;
+        int p=1,s=0;
+        while(n) {
+            s+=(n%10);
+            p*=(n%10);
             n=n/10;
         }
        return (p-s);
