bfa09b3467e2b242164814dd099fcee58fb45913
Rachit Joshi <114751447+Rachit2912@users.noreply.github.com>
2023-09-05 02:31:26 +0530

    Time: 6 ms (36.33%) | Memory: 9.2 MB (40.06%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -5,7 +5,7 @@ public:
         sort(v.begin(),v.end());
         int n=v.size();
         string first=v[0],last=v[n-1];
-        for(int i=0;i<first.size();i++){
+        for(int i=0;i<min(first.size(),last.size());i++){
             if(first[i]!=last[i]){
                 return ans;
             }
