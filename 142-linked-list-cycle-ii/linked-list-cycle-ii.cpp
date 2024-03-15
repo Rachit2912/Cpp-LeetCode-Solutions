f923f993dedc54f2d0f5e035655efdfce0ab1f4c
Rachit Joshi <114751447+Rachit2912@users.noreply.github.com>
2024-03-16 02:01:05 +0530

    Time: 6 ms (70.52%) | Memory: 10 MB (90.34%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -8,27 +8,18 @@
  */
 class Solution {
 public:
-       ListNode *detectCycle(ListNode *head) {
-    if (head == NULL || head->next == NULL) return NULL;
-    
-    ListNode* firstp = head;
-    ListNode* secondp = head;
-    bool isCycle = false;
-    
-    while(firstp != NULL && secondp != NULL) {
-        firstp = firstp->next;
-        if (secondp->next == NULL) return NULL;
-        secondp = secondp->next->next;
-        if (firstp == secondp) { isCycle = true; break; }
+    ListNode *detectCycle(ListNode *head) {
+        ListNode *slow = head, *fast = head;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast) break;
+        }
+        if (!(fast && fast->next)) return NULL;
+        while (head != slow) {
+            head = head->next;
+            slow = slow->next;
+        }
+        return head;
     }
-    
-    if(!isCycle) return NULL;
-    firstp = head;
-    while( firstp != secondp) {
-        firstp = firstp->next;
-        secondp = secondp->next;
-    }
-
-    return firstp;
-}
 };
