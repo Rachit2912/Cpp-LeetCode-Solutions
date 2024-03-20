227868de5b7216b957a9e80de3579912b6794e46
Rachit Joshi <114751447+Rachit2912@users.noreply.github.com>
2024-03-20 22:31:18 +0530

    Time: 186 ms (29.25%) | Memory: 120.5 MB (50.08%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -9,30 +9,57 @@
  * };
  */
 class Solution {
-public:
-
-bool arrPalindrome(vector<int> arr){
-    int n = arr.size();
-    int s = 0;
-    int e = n-1;
-
-    while (s<=e) {
-    if (arr[s] != arr[e]) {
-    return 0;
+    ListNode* getmid(ListNode* head){
+    ListNode *slow=head;
+    ListNode* fast = head->next;
+    while (fast != NULL && fast->next != NULL) {
+    fast = fast->next->next;
+    slow = slow->next;
     }
-    s++; e--;
-    }
-return 1;
-
+    return slow;
 }
 
+ListNode* reverse(ListNode* head){
+    ListNode* curr = head;
+    ListNode* prev = NULL;
+    ListNode* next = NULL;
+    while (curr != NULL) {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+}
+return prev;
+}
+
+public:
     bool isPalindrome(ListNode* head) {
-        vector<int> arr;
-    ListNode* temp = head;
-    while (temp != NULL) {
-    arr.push_back(temp->val);  
-    temp = temp->next;
+        // empty list:
+    if (head->next == NULL) return true;
+
+    // finding middle :
+    ListNode* mid = getmid(head);
+    
+    // reverse sub-ll part after middle :
+    ListNode* temp = mid->next;
+    mid->next = reverse(temp);
+
+    // comparing both half parts :
+    ListNode* head1 = head;
+    ListNode* head2 = mid->next;
+
+    while(head2 != NULL){
+        if (head1->val != head2->val) {
+        return 0;
+        }
+        head1 = head1->next;
+        head2 = head2->next;
     }
-    return arrPalindrome(arr);
+
+    // reverting the ll back to its original position :
+    temp = mid->next;
+    mid->next = reverse(temp);
+
+    return 1;
     }
 };
