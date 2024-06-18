bb1f0272407b13c318b4ae12d643fe75b7e67499
Rachit Joshi <114751447+Rachit2912@users.noreply.github.com>
2024-06-18 12:27:45 +0530

    Time: 85 ms (25.90%) | Memory: 41.1 MB (57.35%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.cpp
+++ 853-most-profit-assigning-work/most-profit-assigning-work.cpp
@@ -1,45 +1,24 @@
 class Solution {
 public:
     int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
-        vector<pair<int,int>> infos;
-        for(int i = 0;i< difficulty.size(); i++){
-            infos.push_back({difficulty[i],profit[i]});
+        priority_queue<pair<int,int>> pq;
+
+        for(int i = 0; i < profit.size(); i++){
+            pq.push({profit[i],difficulty[i]});
         }
-        sort(infos.begin(),infos.end());
 
-        vector<pair<int,int>> maxProfitforDifficulty;
-        int maxp = 0;
-        for(auto info : infos){
-            int d = info.first , p = info.second;
-            maxp = max(maxp,p);
-            maxProfitforDifficulty.push_back({d,maxp});}
+        sort(worker.begin(),worker.end(),greater<int>());
 
-             int totalProfit = 0;
-        for (int w : worker) {
-            int ind = binarySearch(maxProfitforDifficulty, w);
-            if (ind >= 0 && w >= maxProfitforDifficulty[ind].first) {
-                totalProfit += maxProfitforDifficulty[ind].second;
+        int i = 0 ;
+        int maxProfit  = 0;
+        while(i < worker.size() && !pq.empty()){
+            if(worker[i] < pq.top().second) pq.pop();
+            else {
+                maxProfit += pq.top().first ; 
+                i++ ;
             }
         }
-        return totalProfit;
+        return maxProfit;
 
-        
     }
-
-    private:
-    int binarySearch(vector<pair<int, int>>& list, int target) {
-        int left = 0, right = list.size() - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (list[mid].first <= target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-        return right;
-    }
-
-
-
 };
