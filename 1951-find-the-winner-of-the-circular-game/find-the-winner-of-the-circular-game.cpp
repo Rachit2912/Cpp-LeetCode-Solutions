3b1f61485fb8e82244cf8f7e100cba7533609b78
Rachit Joshi <114751447+Rachit2912@users.noreply.github.com>
2024-07-08 17:08:38 +0530

    Time: 3 ms (65.36%) | Memory: 7.2 MB (76.96%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -1,21 +1,13 @@
 class Solution {
 public:
     int findTheWinner(int n, int k) {
-        return indexFinder(n,k) + 1;
-        // ye original winner ka index lake dega and usme +1 krenge to winner ka name ajayega
-    }
-
-    int indexFinder(int n, int k){
-
-        // base case : last me 1 winner bacha hoga jiska index 0 hi ho skta h 
-        if(n==1) return 0;
-
-        // har step me saare indices -k se shift honge bcz we are deleting a frnd at index 'k' so it'll 
-        // shift all the frnds to a measure of -k
-        // ab dost kam hote jayenge har step me isliye n-1 hota jayega 
-        int index = indexFinder(n-1,k);
-        index = (index+k)%n; // bcz hme usko usi 'n' size k bound me rkhna h & sb -k se shift hue the isliye
-        // wapas +k krke reset krdia 
-        return index;
+        // real winner jb bachega to 0 index pr hoga
+        int winner =0;
+        //ab baaki bache n-1 frnds ko firse jodkr jodkr us winner ka naam nikal lenge
+        for(int i=2;i<=n;i++){
+            winner = (winner+k)%i; // bcz winner n-1 times -k krke shift hota hua pahucha h 0 index pr
+            // or %i isliye kia h bcz us time pr grp me 'i' bande hi honge 
+        }
+        return winner+1;// final index aagya isme +1 krenge bcz we are using 0-indexing
     }
 };
