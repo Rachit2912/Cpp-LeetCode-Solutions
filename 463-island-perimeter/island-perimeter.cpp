68f3f3bd66ed3ab0f6cfbf1c8f1e32c7b3efdd1c
Rachit Joshi <114751447+Rachit2912@users.noreply.github.com>
2024-08-11 16:56:29 +0530

    Time: 87 ms (24.42%) | Memory: 102.6 MB (26.75%) - LeetSync

--- 463-island-perimeter/island-perimeter.cpp
+++ 463-island-perimeter/island-perimeter.cpp
@@ -1,24 +1,24 @@
 class Solution {
-    void dfs(int x, int y, int r, int c, vector<vector<int>> &grid, vector<vector<bool>> &visited, int &perimeter){
-        visited[x][y] = true;
+    void dfs(int x, int y, int r, int c, vector<vector<int>> &grid, int &perimeter){
+        grid[x][y] = '$';
         int cellPerimeter = 4;
 
         // Check the four possible neighbors and decrease perimeter for shared boundaries
-        if (x > 0 && grid[x-1][y] == 1) { // up
+        if (x > 0 && grid[x-1][y] != 0) { // up
             cellPerimeter--;
-            if (!visited[x-1][y]) dfs(x-1, y, r, c, grid, visited, perimeter);
+            if (grid[x-1][y] == 1) dfs(x-1, y, r, c, grid, perimeter);
         }
-        if (x < r-1 && grid[x+1][y] == 1) { // down
+        if (x < r-1 && grid[x+1][y] != 0) { // down
             cellPerimeter--;
-            if (!visited[x+1][y]) dfs(x+1, y, r, c, grid, visited, perimeter);
+            if (grid[x+1][y] == 1) dfs(x+1, y, r, c, grid, perimeter);
         }
-        if (y > 0 && grid[x][y-1] == 1) { // left
+        if (y > 0 && grid[x][y-1] != 0) { // left
             cellPerimeter--;
-            if (!visited[x][y-1]) dfs(x, y-1, r, c, grid, visited, perimeter);
+            if (grid[x][y-1] == 1) dfs(x, y-1, r, c, grid, perimeter);
         }
-        if (y < c-1 && grid[x][y+1] == 1) { // right
+        if (y < c-1 && grid[x][y+1] != 0) { // right
             cellPerimeter--;
-            if (!visited[x][y+1]) dfs(x, y+1, r, c, grid, visited, perimeter);
+            if (grid[x][y+1] == 1) dfs(x, y+1, r, c, grid, perimeter);
         }
 
         perimeter += cellPerimeter;
@@ -29,12 +29,11 @@ public:
         int r = grid.size();
         int c = grid[0].size();
         int perimeter = 0;
-        vector<vector<bool>> visited(r, vector<bool>(c, false));
         
         for(int i = 0; i < r; i++){
             for(int j = 0; j < c; j++){
-                if(grid[i][j] == 1 && !visited[i][j]){
-                    dfs(i, j, r, c, grid, visited, perimeter);
+                if(grid[i][j] == 1){
+                    dfs(i, j, r, c, grid, perimeter);
                 }
             }
         }
