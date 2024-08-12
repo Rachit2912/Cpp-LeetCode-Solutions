1cba779c9cfa9d8439fa302d41f82e7987c72a87
Rachit Joshi <114751447+Rachit2912@users.noreply.github.com>
2024-08-12 14:12:45 +0530

    Time: 24 ms (6.61%) | Memory: 14.1 MB (20.01%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        unordered_map<char, int> m;
-        
-        m['I'] = 1;
-        m['V'] = 5;
-        m['X'] = 10;
-        m['L'] = 50;
-        m['C'] = 100;
-        m['D'] = 500;
-        m['M'] = 1000;
-        
-        int ans = 0;
-        
-        for(int i = 0; i < s.length(); i++){
-            if(m[s[i]] < m[s[i+1]]){
-                ans -= m[s[i]];
-            }
-            else{
-                ans += m[s[i]];
-            }
-        }
+        size_t pos;
+
+    while ((pos = s.find("IV")) != std::string::npos) s.replace(pos, 2, "a"); 
+    while ((pos = s.find("IX")) != std::string::npos) s.replace(pos, 2, "b"); 
+    while ((pos = s.find("XL")) != std::string::npos) s.replace(pos, 2, "c"); 
+    while ((pos = s.find("XC")) != std::string::npos) s.replace(pos, 2, "d"); 
+    while ((pos = s.find("CD")) != std::string::npos) s.replace(pos, 2, "e"); 
+    while ((pos = s.find("CM")) != std::string::npos) s.replace(pos, 2, "f"); 
+
+    std::unordered_map<char, int> roman_to_value = {
+        {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50},
+        {'C', 100}, {'D', 500}, {'M', 1000},
+        {'a', 4},  
+        {'b', 9},  
+        {'c', 40}, 
+        {'d', 90}, 
+        {'e', 400},
+        {'f', 900} 
+    };
+
+    int ans = 0;
+    for (auto i : s){
+        ans += roman_to_value[i];
+    }
         return ans;
     }
 };
