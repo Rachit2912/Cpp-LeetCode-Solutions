cbf127f9f499f189ee71188f11391c451ee3e2c9
Rachit Joshi <114751447+Rachit2912@users.noreply.github.com>
2024-08-28 14:37:14 +0530

    Time: 197 ms (91.49%) | Memory: 94.8 MB (69.62%) - LeetSync

--- 2035-count-sub-islands/count-sub-islands.cpp
+++ 2035-count-sub-islands/count-sub-islands.cpp
@@ -1,49 +1,33 @@
 class Solution {
-
-        vector<pair<int,int>> directions =  {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-
-
     private : 
-    bool bfs(vector<vector<int>> &grid1, vector<vector<int>> &grid2, int x, int y ){
+    bool dfs(vector<vector<int>>& grid1, vector<vector<int>> &grid2, int x, int y){
 
-        bool result = true;
         int m = grid2.size();
         int n = grid2[0].size();
 
-        queue<pair<int,int>> q;
-        q.push({x,y});
+        if(x<0 || x>=m || y<0 || y>=n) return 1;
+        if(grid2[x][y] != 1) return 1;
+
         grid2[x][y] = -1;
 
+        bool result = (grid1[x][y] ==1);
+        result = result & dfs(grid1,grid2,x+1,y) & dfs(grid1,grid2,x-1,y) & dfs(grid1,grid2,x,y+1) & dfs(grid1,grid2,x,y-1);
 
-        while(!q.empty()){
-            auto [x,y] = q.front();
-            q.pop();
-
-            if(grid1[x][y] != 1){
-                result = false;
-            }
-
-            for(auto i :directions){
-                int nx = x+i.first;
-                int ny = y+i.second;
-
-                if(nx>=0 && nx<m && ny>=0 && ny<n && grid2[nx][ny] ==1 ){
-                    grid2[nx][ny] = -1;
-                    q.push({nx,ny});
-                }
-            }
-        }
         return result;
     }
 public:
-
     int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
+        int m = grid2.size();
+        int n = grid2[0].size();
         int count = 0;
-        for(int i =0; i<grid2.size(); i++){
-            for(int j =0; j<grid2[0].size();j++){
-            if(grid2[i][j] == 1){count += bfs(grid1,grid2,i,j);}
+
+        for(int i =0; i<m;i++){
+            for(int j =0; j<n; j++){
+                if(grid2[i][j] == 1){
+                    count+= dfs(grid1,grid2,i,j);
+                }
+            }
         }
-        }
-        return count; 
+        return count;
     }
 };
