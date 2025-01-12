f74512cb81b9aa7f1b89950b9ce9d722781a91ff
Rachit Joshi <114751447+Rachit2912@users.noreply.github.com>
2025-01-12 18:28:48 +0530

    Time: 15 ms (51.52%) | Memory: 30.2 MB (53.05%) - LeetSync

--- 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.cpp
+++ 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.cpp
@@ -1,31 +1,21 @@
 class Solution {
 public:
     bool canBeValid(string s, string locked) {
+        int open(0),close(0);
         int n = s.length();
         if(n%2==1)return false;
-        stack<int> open,openClose;
 
         for(int i=0; i<n; i++){
-            if(locked[i]=='0'){openClose.push(i);}
-
-            else{
-                if(s[i]=='('){open.push(i);}
-
-                else{
-                    if(!open.empty()){open.pop();}
-                    else if(!openClose.empty()){openClose.pop();}
-                    else{return false;}
-                }
-
-            }
+            if(s[i]=='(' || locked[i]=='0')open++;
+            else open--;
+            if(open<0)return false;
         }
 
-
-        while(!open.empty() && !openClose.empty() && (open.top()<openClose.top())){
-            open.pop();
-            openClose.pop();
+        for(int i=n-1; i>=0; i--){
+            if(s[i]==')' || locked[i]=='0')close++;
+            else{close--;}
+            if(close<0)return false;
         }
-
-        return open.empty();
+        return true;      
     }
 };
