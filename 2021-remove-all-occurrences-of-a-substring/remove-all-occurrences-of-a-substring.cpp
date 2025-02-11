e4240d39763b8a781cd8005315e1153196460a9f
Rachit Joshi <114751447+Rachit2912@users.noreply.github.com>
2025-02-11 14:17:06 +0530

    Time: 16 ms (5.85%) | Memory: 29.2 MB (5.32%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -1,10 +1,31 @@
 class Solution {
+    private:
+    bool checkMatch(stack<char> &st, string part, int p){
+        stack<char> temp = st;
+        for(int i=p-1; i>=0; i--){
+            if(temp.top() != part[i])return false;
+
+            temp.pop();
+        }
+        return true;
+    }
 public:
     string removeOccurrences(string s, string part) {
-        while(s.find(part) != -1){
-            int i = s.find(part);
-            s = s.substr(0,i) + s.substr(i+part.length());
+        int n = s.length(), p = part.length();
+        stack<char> st;
+        for(int i=0; i<n; i++){
+            st.push(s[i]);
+            if(st.size()>=p && checkMatch(st,part,p)){
+                for(int j=0; j<p; j++)st.pop();
+            }
         }
+
+        s = "";
+        while (!st.empty()) {
+            s = st.top() + s;
+            st.pop();
+        }
+
         return s;
     }
 };
