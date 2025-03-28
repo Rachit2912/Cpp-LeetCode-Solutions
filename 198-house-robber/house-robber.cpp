564e2812f4e3b21d89a9a221eb92c43af302f282
Rachit Joshi <114751447+Rachit2912@users.noreply.github.com>
2025-03-28 19:16:39 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (78.25%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -4,12 +4,14 @@ public:
         int n = nums.size();
         if(n==0)return 0;
         if(n==1)return nums[0];
-        vector<int> dp(n+1,-1);
-        dp[0]=nums[0];dp[1]=max(nums[0],nums[1]);
+        int prev=nums[0],prev2=max(nums[0],nums[1]);
+        int curr =0;
         for(int i=2; i<n; i++){
-            dp[i]=max(dp[i-1],dp[i-2]+nums[i]);
+            curr=max(prev2,prev+nums[i]);
+            prev=prev2;
+            prev2=curr;
         }
-        return dp[n-1];
+        return prev2;
 
     }
 };
