376372a9c55d839305fc623918f643b9053f8670
Rachit Joshi <114751447+Rachit2912@users.noreply.github.com>
2025-04-02 18:51:22 +0530

    Time: 0 ms (100.00%) | Memory: 19.8 MB (84.66%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,14 +1,28 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int,int> freq;
-        for(auto &n : nums)freq[n]++;
+        int cnt1=0,cand1=NULL,cnt2=0,cand2=NULL;
+        int n = nums.size();
 
-        int n = nums.size()/3;
-        vector<int>ans;
-        for(auto &f : freq){
-            if(f.second>n)ans.push_back(f.first);
+        for(int n : nums){
+            if(n==cand1)cnt1++;
+            else if(n==cand2)cnt2++;
+            else if(cnt1==0){cand1=n;cnt1=1;}
+            else if(cnt2==0){cand2=n;cnt2=1;}
+            else {cnt1--;cnt2--;}
         }
+
+        // validation part :
+        vector<int> ans;
+        int freq1=0,freq2=0;
+        for(int n : nums){
+            if(n==cand1)freq1++;
+            else if(n==cand2)freq2++;
+        }
+
+        if(freq1 > floor(n/3))ans.push_back(cand1);
+        if(freq2> floor(n/3))ans.push_back(cand2);
+
         return ans;
     }
 };
