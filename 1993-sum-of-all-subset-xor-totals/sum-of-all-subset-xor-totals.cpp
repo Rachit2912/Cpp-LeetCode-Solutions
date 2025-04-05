3afe7538552bbda8db46306155b679fcc6acf6be
Rachit Joshi <114751447+Rachit2912@users.noreply.github.com>
2025-04-05 17:42:38 +0530

    Time: 60 ms (10.45%) | Memory: 31.3 MB (11.28%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
@@ -1,39 +1,18 @@
 class Solution {
 public:
     int subsetXORSum(vector<int>& nums) {
-        
-        vector<vector<int>> subsets;
-    generateSubsets(0, subsets, {}, nums);
-
-    int ans = 0;
-    for (auto& s : subsets) {
-        int xors = 0;
-        for (int n : s)
-            xors ^= n;
-        ans += xors;
+        return xorsum(0,nums,0);
     }
-    return ans;
-}
 
-private : 
-    void generateSubsets(int idx, vector<vector<int>>& subsets,
-                          vector<int> temp, vector<int> nums) {
+private:
+    int xorsum(int idx, vector<int> nums, int currXOR){
+        
+        // base case:
+        if(idx==nums.size())return currXOR;
 
-                            // base case :
-                            if(idx == nums.size()){
-                                subsets.push_back(temp);
-                                return;
-                            }
+        int noskip = xorsum(idx+1,nums,currXOR^nums[idx]);
+        int skip = xorsum(idx+1,nums,currXOR);
 
-                            // explore :
-                            temp.push_back(nums[idx]);
-                            generateSubsets(idx+1,subsets,temp,nums);
-                            //backtrack:
-                            temp.pop_back();
-
-                            // skip and explore:
-                            generateSubsets(idx+1,subsets,temp,nums);
-
-                          }
-}
-;
+        return (noskip+skip);
+    }
+};
