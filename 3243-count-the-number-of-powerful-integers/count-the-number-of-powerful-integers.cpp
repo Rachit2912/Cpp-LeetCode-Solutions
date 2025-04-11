8e1fba6d183ff41897eecf3d1c275b5922cc41e0
Rachit Joshi <114751447+Rachit2912@users.noreply.github.com>
2025-04-12 01:21:52 +0530

    Time: 7 ms (28.29%) | Memory: 11.9 MB (15.12%) - LeetSync

--- 3243-count-the-number-of-powerful-integers/count-the-number-of-powerful-integers.cpp
+++ 3243-count-the-number-of-powerful-integers/count-the-number-of-powerful-integers.cpp
@@ -2,34 +2,34 @@
 class Solution {
 public:
     long long numberOfPowerfulInt(long long start, long long finish, int limit, string s) {
-        ll result = countUpTo(to_string(finish),s,limit)-countUpTo(to_string(start-1),s,limit);
-        return result;
-    }
+        auto countUpTo = [&](ll num) -> ll {
+            string numStr = to_string(num);
+            int suffixLen = s.size();
+            int prefixLen = numStr.size()-suffixLen;
 
-private:
-    long long countUpTo(const string& numStr, string suffix, int limit){
-        int numLen = numStr.length();
-        int suffixLen = suffix.length();
+            if(prefixLen < 0)return 0;
+            
+            vector<vector<ll>> dp(prefixLen+1,vector<ll>(2,0));
 
-        if(numLen < suffixLen)return 0;
-        if(numLen == suffixLen)return (numStr >= suffix) ? 1 : 0 ;
+            dp[prefixLen][0] = 1;
+            string suffixFromNum = numStr.substr(prefixLen);
+            dp[prefixLen][1] = stoll(suffixFromNum) >= stoll(s) ? 1 : 0;
 
-        long long count=0;
-        int prefixLen = numLen-suffixLen;
-        string actualSuffix = numStr.substr(prefixLen);
+            for(int i=prefixLen-1; i>=0; i--){
+                int digit = numStr[i]-'0';
 
-        for(int i=0; i<prefixLen; i++){
-            long long waysForRemaining = pow(limit+1,prefixLen-i-1);
-            int currDigit = numStr[i]-'0';
-            if(currDigit>limit){
-                count += (limit+1)*waysForRemaining;
-                return count;
+                dp[i][0] = (limit+1) * dp[i+1][0];
+
+                if(digit <= limit){
+                    dp[i][1] = digit*dp[i+1][0] + dp[i+1][1];
+                }else{
+                    dp[i][1] = (limit+1)*dp[i+1][0];
+                }
             }
-            count += (currDigit*waysForRemaining);
-        }
 
-        if(actualSuffix >= suffix)count++;
+            return dp[0][1];
+        };
 
-        return count;
+        return countUpTo(finish)-countUpTo(start-1);
     }
 };
