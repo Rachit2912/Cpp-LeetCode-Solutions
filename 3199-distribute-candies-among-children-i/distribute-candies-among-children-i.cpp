0c3b8d8d509ad8581e249593a756d3b2b76c3e91
Rachit Joshi <114751447+Rachit2912@users.noreply.github.com>
2025-06-01 11:41:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (59.66%) - LeetSync

--- 3199-distribute-candies-among-children-i/distribute-candies-among-children-i.cpp
+++ 3199-distribute-candies-among-children-i/distribute-candies-among-children-i.cpp
@@ -2,13 +2,11 @@ class Solution {
 public:
     int distributeCandies(int n, int limit) {
            int cnt=0;
-        for(int i=0; i<=limit; i++){
+       for(int i=0; i<=limit; i++){
             for(int j=0; j<=limit; j++){
-                for(int k=0; k<=limit; ++k){
-                    if(i+j+k == n)cnt++;
+                    if(n-i-j <= limit && n-i-j>=0)cnt++;
                 }
             }
-        }
         return cnt;
     }
 };
