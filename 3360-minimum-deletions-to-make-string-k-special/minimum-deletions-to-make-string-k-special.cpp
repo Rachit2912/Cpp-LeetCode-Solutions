e62d191e21744593f79c0061dc270f10e8e12e0c
Rachit Joshi <114751447+Rachit2912@users.noreply.github.com>
2025-06-21 20:40:42 +0530

    Time: 1 ms (89.13%) | Memory: 19.4 MB (55.07%) - LeetSync

--- 3360-minimum-deletions-to-make-string-k-special/minimum-deletions-to-make-string-k-special.cpp
+++ 3360-minimum-deletions-to-make-string-k-special/minimum-deletions-to-make-string-k-special.cpp
@@ -3,20 +3,22 @@ public:
     int minimumDeletions(string word, int k) {
         vector<int> freq(26,0);
         for(auto w : word)freq[w-'a']++;
+        sort(begin(freq),freq.end());
         int ans = word.size();
+        int deletionsB4i = 0;
 
         for(int i=0; i<26; i++){
-            int deletions = 0;
+            int currDeletions = deletionsB4i;
             
-            for(int j=0; j<26; j++){
-                
-                if(i==j) continue;
+            for(int j=25; j>-1; j--){
 
-                if(freq[j] < freq[i])deletions += freq[j];
-                else if(abs(freq[j] - freq[i]) > k)deletions += (abs(freq[j]-freq[i])-k);
+                if(freq[j]-freq[i] <= k)break;
+
+                currDeletions += (freq[j]-freq[i]-k);
             }
 
-            ans = min(deletions,ans);
+            ans = min(currDeletions,ans);
+            deletionsB4i += freq[i];
         }
 
         return ans;
