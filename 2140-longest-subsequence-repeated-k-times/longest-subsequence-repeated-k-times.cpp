7eef1ac46d55ebbd8d88eff6bdcb5e5bace1199e
Rachit Joshi <114751447+Rachit2912@users.noreply.github.com>
2025-06-28 05:34:39 +0530

    Time: 370 ms (52.86%) | Memory: 10.8 MB (94.29%) - LeetSync

--- 2140-longest-subsequence-repeated-k-times/longest-subsequence-repeated-k-times.cpp
+++ 2140-longest-subsequence-repeated-k-times/longest-subsequence-repeated-k-times.cpp
@@ -1,56 +1,70 @@
 class Solution {
 public:
+    string ans="";
     string longestSubsequenceRepeatedK(string s, int k) {
-        vector<int> freq(26);
-        for (char ch : s) {
-            freq[ch - 'a']++;
-        }
-        vector<char> candidate;
-        for (int i = 25; i >= 0; i--) {
-            if (freq[i] >= k) {
-                candidate.push_back('a' + i);
+        int n = s.length();
+        vector<int> freq(26,0);
+        for(auto ch : s)freq[ch-'a']++;
+
+        vector<bool>canUse(26,false);
+        for(int i=0; i<26; i++){
+            if(freq[i]>=k){
+                canUse[i]=true;
+                freq[i] /= k;
             }
-        }
-        queue<string> q;
-        for (char ch : candidate) {
-            q.push(string(1, ch));
         }
 
-        string ans = "";
-        while (!q.empty()) {
-            string curr = q.front();
-            q.pop();
-            if (curr.size() > ans.size()) {
-                ans = curr;
-            }
-            // generate the next candidate string
-            for (char ch : candidate) {
-                string next = curr + ch;
-                if (isKRepeatedSubsequence(s, next, k)) {
-                    q.push(next);
-                }
-            }
+        int maxLen = n/k;
+
+        for(int len=maxLen; len>=0; len--){
+            vector<int> tempFreq = freq;
+            string curr="";
+            if(genSubseq_and_Chk(s,curr,canUse,tempFreq,k,len))return ans;
         }
 
         return ans;
     }
 
-    bool isKRepeatedSubsequence(const string& s, const string& t, int k) {
-        int pos = 0, matched = 0;
-        int n = s.size(), m = t.size();
-        for (char ch : s) {
-            if (ch == t[pos]) {
-                pos++;
-                if (pos == m) {
-                    pos = 0;
-                    matched++;
-                    if (matched == k) {
-                        return true;
-                    }
-                }
-            }
-        }
+private:
+    bool genSubseq_and_Chk(string &s,string &curr,vector<bool> &canUse,vector<int> &freq,int k, int len){
 
+        // base case 
+        if(curr.length() == len){
+            if(isSubseq(curr,s,k)){
+                ans = curr;
+                return true;
+            }
+            return false;
+        }
+        
+
+        for(int i=25; i>-1; i--){
+            if(canUse[i]==false || freq[i]==0)continue;
+
+            // do 
+            char ch = i+'a';
+            curr.push_back(ch);
+            freq[i]--;
+
+            //explore 
+            if(genSubseq_and_Chk(s,curr,canUse,freq,k,len))return true;
+
+            //undo
+            curr.pop_back();
+            freq[i]++;
+        }
         return false;
     }
+
+    bool isSubseq(string &sub, string &s, int k){
+        int i=0,j=0;
+        int L = sub.length(), n =s.length();
+
+        while(i<n && j<k*L){
+            if(s[i] == sub[j%L])j++;
+            i++;
+        }
+        return j==(L*k);
+    }
+
 };
